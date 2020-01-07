1c4f7888c54f0e84c4ffe320321beb46bee38517
Eduardo Ramos <ramosedward8@gmail.com>
2020-01-07 00:15:08 -0800

    Updates

---
+++ precache-manifest.f17a4e354588d9b1a5828a7af2163ffa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21490b85493bf4d158e6222032852c41",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "950cce1715db3dc36315",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9bc206a4c92299f42db1",
+    "url": "/portfolio/static/js/2.6bd413a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.6bd413a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "950cce1715db3dc36315",
+    "url": "/portfolio/static/js/main.7d63cb27.chunk.js"
+  },
+  {
+    "revision": "e139b2548d94e481a5d0",
+    "url": "/portfolio/static/js/runtime-main.0762ecda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
